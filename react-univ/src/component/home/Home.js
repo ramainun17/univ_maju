6044a3ddfbf866453e43abac4cd801cddc01fc70
ramainun17 <zenaken107@gmail.com>
2023-12-13 09:22:26 +0700

    ubah dikit

--- react-univ/src/component/home/Home.js
+++ react-univ/src/component/home/Home.js
@@ -53,7 +53,7 @@ function Home() {
             <div className="col-sm-6">
               <ol className="breadcrumb float-sm-right">
                 <li className="breadcrumb-item">
-                  <Link to="#">Universitas Maju</Link>
+                  <Link className=" text-decoration-none" to="/">Universitas Maju</Link>
                 </li>
                 <li className="breadcrumb-item active">Dashboard</li>
               </ol>
@@ -66,80 +66,6 @@ function Home() {
       </div>
       {/* /.content-header */}
       {/* Main content */}
-      <section className="content">
-        <div className="container-fluid">
-          {/* Small boxes (Stat box) */}
-          <div className="row">
-            <div className="col-lg-3 col-6">
-              {/* small box */}
-              <div className="small-box bg-info">
-                <div className="inner">
-                  <h3>150</h3>
-                  <p>New Orders</p>
-                </div>
-                <div className="icon">
-                  <i className="ion ion-bag" />
-                </div>
-                <Link to="#" className="small-box-footer">
-                  More info <i className="fas fa-arrow-circle-right" />
-                </Link>
-              </div>
-            </div>
-            {/* ./col */}
-            <div className="col-lg-3 col-6">
-              {/* small box */}
-              <div className="small-box bg-success">
-                <div className="inner">
-                  <h3>
-                    53<sup style={{ fontSize: 20 }}>%</sup>
-                  </h3>
-                  <p>Bounce Rate</p>
-                </div>
-                <div className="icon">
-                  <i className="ion ion-stats-bars" />
-                </div>
-                <Link to="#" className="small-box-footer">
-                  More info <i className="fas fa-arrow-circle-right" />
-                </Link>
-              </div>
-            </div>
-            {/* ./col */}
-            <div className="col-lg-3 col-6">
-              {/* small box */}
-              <div className="small-box bg-warning">
-                <div className="inner">
-                  <h3>44</h3>
-                  <p>User Registrations</p>
-                </div>
-                <div className="icon">
-                  <i className="ion ion-person-add" />
-                </div>
-                <Link to="#" className="small-box-footer">
-                  More info <i className="fas fa-arrow-circle-right" />
-                </Link>
-              </div>
-            </div>
-            {/* ./col */}
-            <div className="col-lg-3 col-6">
-              {/* small box */}
-              <div className="small-box bg-danger">
-                <div className="inner">
-                  <h3>65</h3>
-                  <p>Unique Visitors</p>
-                </div>
-                <div className="icon">
-                  <i className="ion ion-pie-graph" />
-                </div>
-                <Link to="#" className="small-box-footer">
-                  More info <i className="fas fa-arrow-circle-right" />
-                </Link>
-              </div>
-            </div>
-            {/* ./col */}
-          </div>
-        </div>
-        {/* /.container-fluid */}
-      </section>
       <section className="content">
         <div className="container-fluid">
           {/* Small boxes (Stat box) */}
